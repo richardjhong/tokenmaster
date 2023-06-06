56b0f58b672b1625e33a0efa1669c33b881d5ac8
Richard Hong <rhong24@gmail.com>
2023-06-06 12:21:22 -0500

    adding TokenMaster contract List function, adding tests for list function

--- backend/contracts/TokenMaster.sol
+++ backend/contracts/TokenMaster.sol
@@ -4,35 +4,53 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenMaster is ERC721 {
-    address public owner;
-    uint256 public totalOccasions;
-    uint256 public totalSupply;
+  address public owner;
+  uint256 public totalOccasions;
+  uint256 public totalSupply;
 
-    struct Occasion {
-        uint256 id;
-        string name;
-        uint256 cost;
-        uint256 tickets;
-        uint256 maxTickets;
-        string date;
-        string time;
-        string location;
-    }
+  struct Occasion {
+    uint256 id;
+    string name;
+    uint256 cost;
+    uint256 tickets;
+    uint256 maxTickets;
+    string date;
+    string time;
+    string location;
+  }
 
-    mapping(uint256 => Occasion) occasions;
-    mapping(uint256 => mapping(address => bool)) public hasBought;
-    mapping(uint256 => mapping(uint256 => address)) public seatTaken;
-    mapping(uint256 => uint256[]) seatsTaken;
+  mapping(uint256 => Occasion) occasions;
+  mapping(uint256 => mapping(address => bool)) public hasBought;
+  mapping(uint256 => mapping(uint256 => address)) public seatTaken;
+  mapping(uint256 => uint256[]) seatsTaken;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
 
-    constructor(
-        string memory _name,
-        string memory _symbol
-    ) ERC721(_name, _symbol) {
-        owner = msg.sender;
-    }
+  constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+    owner = msg.sender;
+  }
+
+  function list(
+    string memory _name,
+    uint256 _cost,
+    uint256 _maxTickets,
+    string memory _date,
+    string memory _time,
+    string memory _location
+  ) public onlyOwner {
+    totalOccasions++;
+    occasions[totalOccasions] = Occasion(
+      totalOccasions,
+      _name,
+      _cost,
+      _maxTickets,
+      _maxTickets,
+      _date,
+      _time,
+      _location
+    );
+  }
 }
--- backend/test/TokenMaster.ts
+++ backend/test/TokenMaster.ts
@@ -1,12 +1,20 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
+import { Signer } from "ethers";
 import { TokenMaster } from "../typechain-types";
 
 const NAME = "TokenMaster";
 const SYMBOL = "TM";
 
+const OCCASION_NAME = "ETH Texas";
+const OCCASION_COST = ethers.utils.parseUnits("1", "ether");
+const OCCASION_MAX_TICKETS = 100;
+const OCCASION_DATE = "Apr 27";
+const OCCASION_TIME = "10:00AM CST";
+const OCCASION_LOCATION = "Austin, Texas";
+
 describe("TokenMaster", () => {
-  let tokenMaster: TokenMaster, deployer, buyer;
+  let tokenMaster: TokenMaster, deployer: Signer, buyer: Signer;
 
   beforeEach(async () => {
     // Setup accounts
@@ -15,7 +23,20 @@ describe("TokenMaster", () => {
     // Deploy contract
     const TokenMaster = await ethers.getContractFactory("TokenMaster");
     tokenMaster = await TokenMaster.deploy(NAME, SYMBOL);
-    await tokenMaster.deployed();
+
+    // Create an occasion
+    const tx = await tokenMaster
+      .connect(deployer)
+      .list(
+        OCCASION_NAME,
+        OCCASION_COST,
+        OCCASION_MAX_TICKETS,
+        OCCASION_DATE,
+        OCCASION_TIME,
+        OCCASION_LOCATION
+      );
+
+    await tx.wait();
   });
 
   describe("Deployment", () => {
@@ -25,6 +46,17 @@ describe("TokenMaster", () => {
 
     it("Should set the symbol", async () => {
       expect(await tokenMaster.symbol()).to.equal(SYMBOL);
+    });
+
+    it("Should set the owner of the contract", async () => {
+      expect(await tokenMaster.owner()).to.equal(await deployer.getAddress());
+    });
+  });
+
+  describe("Occasions", () => {
+    it("Should update the totalOccasions count", async () => {
+      const beginningTotalOccasions = await tokenMaster.totalOccasions();
+      expect(beginningTotalOccasions).to.be.equal(1);
     });
   });
 });
