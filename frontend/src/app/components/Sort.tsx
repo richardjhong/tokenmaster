c021a03d4c6b71c17997fe231833364eb3928436
Richard Hong <rhong24@gmail.com>
2023-06-12 10:07:35 -0500

    changing naming convention of mapped sortOptions in Sort component, adding logic for displaying contract balance to deployer and updating displayed balance when seat is purchased/reserved

--- frontend/src/app/components/Sort.tsx
+++ frontend/src/app/components/Sort.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const sortTitles = [
+const sortOptions = [
   "Select Your Genre",
   "Select Your Dates",
   "Select Your Distance",
@@ -13,12 +13,12 @@ interface SortProps {
 const Sort: React.FC<SortProps> = ({ contractOwnerConnected }) => {
   return (
     <div className='grid grid-cols-12 grid-rows-2 h-full p-10 text-left transition duration-250 ease hover:bg-stone-200 bg-opacity-20'>
-      {sortTitles.map((title, index) => (
+      {sortOptions.map((option, index) => (
         <div
           key={index}
           className='col-span-3 w-52 h-10 mx-1 my-6 border-2 row-span-2 place-self-center border-black flex justify-between items-center cursor-pointer hover:border-blue-400'
         >
-          <p className='ml-2 text-sm md:text-base'>{title}</p>
+          <p className='ml-2 text-sm md:text-base'>{option}</p>
           <img
             src='./angle-down-solid.svg'
             className='w-3 h-auto mr-2'
@@ -28,6 +28,7 @@ const Sort: React.FC<SortProps> = ({ contractOwnerConnected }) => {
       ))}
       {contractOwnerConnected && (
         <button
+          type='button'
           onClick={() => console.log("ready to work")}
           className='col-span-3 row-span-2 place-self-center w-32 h-10 bg-light-blue text-white ml-auto border-none rounded-md font-open-sans text-base font-semibold cursor-pointer transition-all duration-250 ease bg-blue-600'
         >
