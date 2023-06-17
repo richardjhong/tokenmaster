5289bc9eaff99590ea33a2ee44b01cad5f347168
Richard Hong <rhong24@gmail.com>
2023-06-17 10:30:15 -0500

    changing from double quotations to single quotations for consistency

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { ethers } from "ethers";
-import Link from "next/link";
+import React from 'react';
+import { ethers } from 'ethers';
+import Link from 'next/link';
 
 interface NavbarProps {
   account: string | null;
@@ -10,53 +10,53 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ account, setAccount }) => {
   const connectHandler = async () => {
     const accounts = await (window as any).ethereum.request({
-      method: "eth_requestAccounts",
+      method: 'eth_requestAccounts',
     });
     const account = ethers.utils.getAddress(accounts[0]);
     setAccount(account);
   };
 
   return (
-    <nav className="grid grid-cols-4 items-center">
-      <div className="flex items-center px-75 col-span-3 h-65">
-        <h1 className="text-white text-1.50em italic ml-1 mr-10">
+    <nav className='grid grid-cols-4 items-center'>
+      <div className='flex items-center px-75 col-span-3 h-65'>
+        <h1 className='text-white text-1.50em italic ml-1 mr-10'>
           tokenMaster
         </h1>
         <input
-          type="text"
-          placeholder="Find millions of experiences"
-          className="bg-white bg-opacity-20 bg-no-repeat bg-center text-white h-35 border-none rounded-md mx-5 pl-2 placeholder-white font-open-sans w-60"
+          type='text'
+          placeholder='Find millions of experiences'
+          className='bg-white bg-opacity-20 bg-no-repeat bg-center text-white h-35 border-none rounded-md mx-5 pl-2 placeholder-white font-open-sans w-60'
         />
 
-        <ul className="flex justify-center items-center h-full list-none">
-          <li className="h-full min-w-60">
+        <ul className='flex justify-center items-center h-full list-none'>
+          <li className='h-full min-w-60'>
             <Link
-              href=""
-              className="flex justify-center items-center flex-wrap h-full px-8 text-white font-semibold no-underline hover:bg-blue-700 bg-opacity-30"
+              href=''
+              className='flex justify-center items-center flex-wrap h-full px-8 text-white font-semibold no-underline hover:bg-blue-700 bg-opacity-30'
             >
               Concerts
             </Link>
           </li>
-          <li className="h-full min-w-60">
+          <li className='h-full min-w-60'>
             <Link
-              href=""
-              className="flex justify-center items-center flex-wrap h-full px-8 text-white font-semibold no-underline hover:bg-blue-700 bg-opacity-30"
+              href=''
+              className='flex justify-center items-center flex-wrap h-full px-8 text-white font-semibold no-underline hover:bg-blue-700 bg-opacity-30'
             >
               Sports
             </Link>
           </li>
-          <li className="h-full min-w-60">
+          <li className='h-full min-w-60'>
             <Link
-              href=""
-              className="flex justify-center items-center flex-wrap h-full px-8 text-white font-semibold no-underline hover:bg-blue-700 bg-opacity-30"
+              href=''
+              className='flex justify-center items-center flex-wrap h-full px-8 text-white font-semibold no-underline hover:bg-blue-700 bg-opacity-30'
             >
               Arts & Theater
             </Link>
           </li>
-          <li className="h-full min-w-60">
+          <li className='h-full min-w-60'>
             <Link
-              href=""
-              className="flex justify-center items-center flex-wrap h-full px-8 text-white font-semibold no-underline hover:bg-blue-700 bg-opacity-30"
+              href=''
+              className='flex justify-center items-center flex-wrap h-full px-8 text-white font-semibold no-underline hover:bg-blue-700 bg-opacity-30'
             >
               More
             </Link>
@@ -64,18 +64,18 @@ const Navbar: React.FC<NavbarProps> = ({ account, setAccount }) => {
         </ul>
       </div>
 
-      <div className="flex justify-end col-span-1">
+      <div className='flex justify-end col-span-1'>
         {account ? (
           <button
-            type="button"
-            className="w-200 h-50 ml-auto mr-10 bg-white bg-opacity-20 text-white border-none rounded-md font-open-sans text-1.10em font-semibold cursor-pointer transition-all duration-250 ease-out hover:bg-indigo-900"
+            type='button'
+            className='w-200 h-50 ml-auto mr-10 bg-white bg-opacity-20 text-white border-none rounded-md font-open-sans text-1.10em font-semibold cursor-pointer transition-all duration-250 ease-out hover:bg-indigo-900'
           >
-            {account.slice(0, 6) + "..." + account.slice(38, 42)}
+            {account.slice(0, 6) + '...' + account.slice(38, 42)}
           </button>
         ) : (
           <button
-            type="button"
-            className="w-200 h-50 ml-auto mr-10 bg-white bg-opacity-20 text-white border-none rounded-md font-open-sans text-1.10em font-semibold cursor-pointer transition-all duration-250 ease-out hover:bg-indigo-900"
+            type='button'
+            className='w-200 h-50 ml-auto mr-10 bg-white bg-opacity-20 text-white border-none rounded-md font-open-sans text-1.10em font-semibold cursor-pointer transition-all duration-250 ease-out hover:bg-indigo-900'
             onClick={connectHandler}
           >
             Connect
